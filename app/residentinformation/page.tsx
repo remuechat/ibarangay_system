0a6aa32552c4ee6fc45b68e1c9d87c509d7c2d2c
Wryley155 <johnlloydmanilag15@gmail.com>
2025-11-16 09:08:50 +0800

    added ResidentInformation

---
+++ app/residentinformation/page.tsx
@@ -0,0 +1,8 @@
+export default function ResidentInformationPage() {
+  return (
+    <div>
+      <h1>Resident Information</h1>
+      <p>This is the Resident Information page.</p>
+    </div>
+  );
+}
